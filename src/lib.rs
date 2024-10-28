8a4a404e16914cddec7a2f01ed3bad55da404a51
kiwislob <kiwislob@gmail.com>
2024-10-29 00:00:14 +1300

    fixed tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,46 +2,45 @@
 // filename[tag tag tag].ext
 use std::collections::BTreeSet;
 use std::convert::{From, TryFrom};
+use std::ffi::OsString;
 use std::path::PathBuf;
 use std::str::FromStr;
 
-const OPEN_BRACE: u8 = b'[';
-const CLOSE_BRACE: u8 = b']';
-const DOT: u8 = b'.';
-const SPACE: u8 = b' ';
-const COMMA: u8 = b',';
-
-type Tag = Vec<u8>;
+type Tag = OsString;
 
 #[derive(Debug)]
 pub struct NameTag {
-    prefix: Vec<u8>,
-    suffix: Vec<u8>,
+    start: usize,
+    stop: usize,
     tags: BTreeSet<Tag>,
+    name: OsString,
 }
 
 // Interface into tag naming scheme. eg filename[tag1 tag2].ext
 impl NameTag {
-    pub fn new<T: Into<Vec<u8>>>(name: T) -> Self {
+    pub fn new<T: Into<OsString>>(name: T) -> Self {
         let data = name.into();
+        let bytes = data.as_encoded_bytes();
         let mut tags = BTreeSet::new();
-        let (prefix, suffix) = match Self::get_tag_bounds(&data) {
+        let (start, stop) = match Self::get_tag_bounds(&bytes) {
             Some((upper, lower)) => {
-                Self::parse_tags(&mut tags, &data[upper + 1..lower - 1]);
-                // TODO: add tags discovered
-                (data[..upper].to_vec(), data[lower..].to_vec())
+                let blah =
+                    unsafe { OsString::from_encoded_bytes_unchecked(bytes[upper..lower].to_vec()) };
+                Self::parse_tags(&mut tags, &bytes[upper + 1..lower - 1]);
+                (upper, lower)
             }
             _ => {
                 // No existing tags. Pick a spot for potential tag insertion.
                 // Before the first period, or at the end of the name entirely.
-                let split = Self::get_ext_bound(&data);
-                (data[..split].to_vec(), data[split..].to_vec())
+                let split = Self::get_ext_bound(&bytes);
+                (split, split)
             }
         };
         Self {
-            prefix,
-            suffix,
+            start,
+            stop,
             tags,
+            name: data,
         }
     }
 
@@ -55,6 +54,7 @@ impl NameTag {
         self.tags.remove(&tag.into());
     }
 
+    /// Grab all tags present
     pub fn get_tags(&self) -> std::collections::btree_set::Iter<Tag> {
         self.tags.iter()
     }
@@ -62,55 +62,42 @@ impl NameTag {
     /// Remove all tags.
     pub fn clear_tags(&mut self) {
         self.tags.clear();
+        let bytes = self.name.as_encoded_bytes();
+        let prefix = bytes[..self.start].iter();
+        let suffix = bytes[self.stop..].iter();
+        self.name = unsafe {
+            OsString::from_encoded_bytes_unchecked(prefix.chain(suffix).copied().collect())
+        };
     }
 
-    fn get_tag_bounds(data: &Vec<u8>) -> Option<(usize, usize)> {
-        let mut level = 0;
-        let mut last_level = 0;
-        let mut upper_bound = 0;
-        for (i, ch) in data.iter().enumerate() {
-            level += match *ch {
-                OPEN_BRACE => 1,
-                CLOSE_BRACE => -1,
-                _ => 0,
-            };
-            if last_level < level {
-                upper_bound = i;
+    // Get the in and out of the tag space. eg [ and ]
+    fn get_tag_bounds(data: &[u8]) -> Option<(usize, usize)> {
+        if let Some(start) = data.iter().position(|x| *x == b'[') {
+            if let Some(rstop) = data.iter().rev().position(|x| *x == b']') {
+                let stop = data.len() - rstop;
+                if start < stop {
+                    return Some((start, stop));
+                }
             }
-            if last_level > level {
-                return Some((upper_bound, i + 1));
-            }
-            last_level = level;
         }
         None
     }
 
     // Find first period, else end of name
-    fn get_ext_bound(data: &Vec<u8>) -> usize {
-        for (i, ch) in data.iter().enumerate() {
-            if *ch == DOT {
-                return i;
-            }
+    fn get_ext_bound(data: &[u8]) -> usize {
+        if let Some(index) = data.iter().position(|x| *x == b'.') {
+            return index;
         }
         data.len()
     }
 
+    // Extract tags from name
     fn parse_tags(tags: &mut BTreeSet<Tag>, data: &[u8]) {
-        let mut buffer = Vec::new();
-        for ch in data.iter() {
-            match *ch {
-                COMMA | SPACE => {
-                    if buffer.len() != 0 {
-                        tags.insert(buffer.clone());
-                        buffer.clear();
-                    }
-                }
-                ch => buffer.push(ch),
-            }
-        }
-        if 0 < buffer.len() {
-            tags.insert(buffer);
-        }
+        let names = data
+            .split(|x| x.is_ascii_whitespace() || *x == b',' || *x == b'[' || *x == b']')
+            .filter(|x| x.len() != 0)
+            .map(|x| unsafe { OsString::from_encoded_bytes_unchecked(x.to_vec()) });
+        tags.extend(names);
     }
 }
 
@@ -118,32 +105,31 @@ impl FromStr for NameTag {
     type Err = &'static String;
 
     fn from_str(name: &str) -> Result<Self, Self::Err> {
-        Ok(NameTag::new(name))
+        Ok(NameTag::new(OsString::from(name)))
     }
 }
 
 impl From<NameTag> for Vec<u8> {
     fn from(nametag: NameTag) -> Self {
-        let prefix = nametag.prefix.into_iter();
-        let suffix = nametag.suffix.into_iter();
         let tag_len = nametag.tags.len();
         if tag_len == 0 {
-            prefix.chain(suffix).collect()
+            nametag.name.as_encoded_bytes().to_vec()
         } else {
-            let open_brace = Some(OPEN_BRACE).into_iter();
-            let close_brace = Some(CLOSE_BRACE).into_iter();
-            let tags = nametag.tags.into_iter().enumerate().flat_map(|(i, t)| {
-                if i + 1 == tag_len {
-                    t.into_iter().chain(None.into_iter())
-                } else {
-                    t.into_iter().chain(Some(SPACE).into_iter())
-                }
-            });
+            let bytes = nametag.name.as_encoded_bytes();
+            let prefix = bytes[..nametag.start].iter();
+            let suffix = bytes[nametag.stop..].iter();
+            let tags = nametag
+                .tags
+                .into_iter()
+                .collect::<Vec<_>>()
+                .join(&OsString::from(" "));
+
             prefix
-                .chain(open_brace)
-                .chain(tags)
-                .chain(close_brace)
+                .chain(b"[".into_iter())
+                .chain(tags.as_encoded_bytes().iter())
+                .chain(b"]".into_iter())
                 .chain(suffix)
+                .copied()
                 .collect()
         }
     }
@@ -151,7 +137,7 @@ impl From<NameTag> for Vec<u8> {
 
 impl From<PathBuf> for NameTag {
     fn from(path: PathBuf) -> NameTag {
-        NameTag::new(path.as_os_str().as_bytes())
+        NameTag::new(path.as_os_str())
     }
 }
 
@@ -192,7 +178,7 @@ mod tests {
     #[test]
     fn test_round_trip_empty_tags() {
         let name_tag: NameTag = "somefile[].txt".parse().unwrap();
-        assert_eq!("somefile.txt", &String::try_from(name_tag).unwrap());
+        assert_eq!("somefile[].txt", &String::try_from(name_tag).unwrap());
     }
 
     // Functionality
@@ -201,10 +187,7 @@ mod tests {
         let name_tag: NameTag = "somefile[tagB tagA].txt".parse().unwrap();
         assert_eq!(
             vec!["tagA", "tagB"],
-            name_tag
-                .get_tags()
-                .map(|t| String::from_utf8_lossy(t))
-                .collect::<Vec<_>>()
+            name_tag.get_tags().collect::<Vec<_>>()
         );
     }
     #[test]
@@ -214,10 +197,7 @@ mod tests {
         name_tag.add_tag("tagA");
         assert_eq!(
             vec!["tagA", "tagB"],
-            name_tag
-                .get_tags()
-                .map(|t| String::from_utf8_lossy(t))
-                .collect::<Vec<_>>()
+            name_tag.get_tags().collect::<Vec<_>>()
         );
     }
     #[test]
@@ -256,7 +236,7 @@ mod tests {
     fn test_round_trip_nested_braces() {
         let name_tag: NameTag = "somefile[nottag [tagB tagA]].txt".parse().unwrap();
         assert_eq!(
-            "somefile[nottag [tagA tagB]].txt",
+            "somefile[nottag tagA tagB].txt",
             &String::try_from(name_tag).unwrap()
         );
     }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use structopt::StructOpt;
 
 use nametag::NameTag;
 
-
-
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Work with tags on files, using a defined format.")]
 enum Cli {
@@ -15,21 +13,23 @@ enum Cli {
         #[structopt(parse(from_os_str))]
         paths: Vec<PathBuf>,
     },
-    Remove {
-    },
-    Query {
-    },
+    Remove {},
+    Query {},
 }
-
-
 
 fn main() {
     match Cli::from_args() {
-        Cli::Add{tags, paths} => {
-            let nametags = paths.iter().map(|path| NameTag::new(path));
-            println!(">tags: {:?}, paths {:?}", tags, paths);
-        },
-        Cli::Remove{} => {},
-        Cli::Query{} => {},
+        Cli::Add { tags, paths } => {
+            let nametags = paths
+                .iter()
+                .map(|path| NameTag::new(path))
+                .collect::<Vec<_>>();
+            println!(
+                ">tags: {:?}, paths {:?}, nametags {:?}",
+                tags, paths, nametags
+            );
+        }
+        Cli::Remove {} => {}
+        Cli::Query {} => {}
     }
 }
